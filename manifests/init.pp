c8d45d7a5a96a17502edb981414c9203d18b2406
Bruno Léon <bruno.leon@savoirfairelinux.com>
2011-12-28 19:24:12 -0500

    Added dependencies on Puppet ssl components. Fix tio allow for password change. Do not use ecryptfs unecessarily. Allow tls_chain for TLS setup.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,19 +4,19 @@ class sssd (
   $use_ecryptfs = false
   ) {
   include ssl::variables
-  
+
   #Natty ssd package is not working
   if $::lsbdistcodename == 'natty' {
     Class['apt::repository::ppa::fabricesp::experimental'] -> Package['sssd']
   }
-  
+
   if $::lsbdistid in ['Ubuntu', 'Debian'] {
     file { '/etc/security/group.conf':
       ensure  => file,
       content => template('sssd/group.conf.erb'),
     }
   }
-  
+
   package { ['sssd', 'libnss-sss', 'libpam-sss', 'auth-client-config'] :ensure => present }
 
   concat { '/etc/sssd/sssd.conf':
@@ -25,7 +25,7 @@ class sssd (
     require => Package['sssd'],
     notify  => Service['sssd'],
   }
-  
+
   concat::fragment { 'sssd.conf.base':
     target  => '/etc/sssd/sssd.conf',
     order   => 02,
@@ -35,16 +35,16 @@ class sssd (
   file { '/etc/auth-client-config/profile.d/sss':
     ensure  => file,
     content => template('sssd/sss.erb'),
-    require => Package['auth-client-config'],    
+    require => Package['auth-client-config'],
     notify  => Exec['update-sssd-profile'],
   }
-  
+
   exec { 'update-sssd-profile':
     command     => 'auth-client-config -a -p sss',
     require     => [ File['/etc/auth-client-config/profile.d/sss'], Package['sssd', 'libnss-sss', 'libpam-sss'] ],
     refreshonly => true,
   }
-  
+
   service { 'sssd':
     ensure    => running,
     enable    => true,
@@ -56,7 +56,7 @@ class sssd (
     package { 'libpam-script':
       ensure => present
     }
-    
+
     file { '/usr/share/libpam-script/pam_script_ses_open':
      ensure  => file,
      source  => 'puppet:///modules/sssd/pam_script_ses_open',
